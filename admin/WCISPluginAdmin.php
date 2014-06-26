334a7f05a3f3948e512d50b57a554b87a3a85129
instantsearchplus <instantsearchplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-26 08:02:23 +0000

    fix - 'setting' link on plugins page to InstantSearch+ dashboard
    git-svn-id: https://plugins.svn.wordpress.org/instantsearch-for-woocommerce/trunk@938920 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/WCISPluginAdmin.php
+++ admin/WCISPluginAdmin.php
@@ -74,10 +74,6 @@ class WCISPluginAdmin {
 		// Add the options page and menu item.
 		add_action( 'admin_menu', array( $this, 'add_plugin_admin_menu' ) );
 
-		// Add an action link pointing to the options page.
-		$plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_slug . '.php' );
-		add_filter( 'plugin_action_links_' . $plugin_basename, array( $this, 'add_action_links' ) );
-
 		/*
 		 * Define custom functionality.
 		 *
@@ -213,21 +209,6 @@ class WCISPluginAdmin {
 	    include_once( 'views/admin.php' );
 	}
 
-	/**
-	 * Add settings action link to the plugins page.
-	 *
-	 * @since    1.0.0
-	 */
-	public function add_action_links( $links ) {
-
-		return array_merge(
-			array(
-				'settings' => '<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_slug ) . '">' . __( 'Settings', $this->plugin_slug ) . '</a>'
-			),
-			$links
-		);
-
-	}
 
 	/**
 	 * NOTE:     Actions are points in the execution of a page or process
