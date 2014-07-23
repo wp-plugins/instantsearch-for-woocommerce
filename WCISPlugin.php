2ef275258cb4a8396930451ab1bfd33cc53cfc3c
instantsearchplus <instantsearchplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-23 10:00:16 +0000

    version change
    git-svn-id: https://plugins.svn.wordpress.org/instantsearch-for-woocommerce/trunk@953355 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- WCISPlugin.php
+++ WCISPlugin.php
@@ -9,7 +9,7 @@
  * Plugin Name:       InstantSearch+ for WooCommerce
  * Plugin URI:        www.instantsearchplus.com 
  * Description:       Best search plugin for WooCommerce
- * Version:           1.1.0
+ * Version:           1.1.1
  * Author:            Fast Simon Inc
  * Author URI:        www.instantsearchplus.com
  * Text Domain:       WCISPlugin
